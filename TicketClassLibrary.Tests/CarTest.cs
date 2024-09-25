0b5927409b29a1ec9b7204230529351be79e4eab
SebastianZealand <zealand@tbrinch.com>
2024-09-25 19:53:27 +0200

    Added testproject.

---
+++ TicketClassLibrary.Tests/CarTest.cs
@@ -0,0 +1,17 @@
+using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicketClassLibrary;
+
+namespace TicketClassLibrary.Tests;
+
+[TestClass]
+[TestSubject(typeof(Car))]
+public class CarTest
+{
+
+    [TestMethod]
+    public void METHOD()
+    {
+        
+    }
+}
